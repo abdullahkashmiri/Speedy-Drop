babe98ad8a53edcf7e1e186fc7a9bc30f36a35a2
abdullahkashmiri <darabdullah380@gmail.com>
2024-03-16 18:07:45 +0500

    Initial Commit 16/03/2024 4th

---
+++ lib/Screens/Home/homeRider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreenRider extends StatefulWidget {
+  const HomeScreenRider({super.key});
+
+  @override
+  State<HomeScreenRider> createState() => _HomeScreenRiderState();
+}
+
+class _HomeScreenRiderState extends State<HomeScreenRider> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
