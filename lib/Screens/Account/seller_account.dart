05334459c24f921c6cd16b2028def24278020f7c
abdullahkashmiri <darabdullah380@gmail.com>
2024-03-30 17:05:15 +0500

    Initial Commit 30/03/2024 2st

---
+++ lib/Screens/Account/seller_account.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SellerAccount extends StatelessWidget {
+  const SellerAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
