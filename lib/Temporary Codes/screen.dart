dec57b497aba7082b2ac437935316cab973ba4eb
Momin-219 <123032309+Momin-219@users.noreply.github.com>
2024-05-14 00:57:36 +0500

    Update screen.dart

--- lib/Temporary Codes/screen.dart
+++ lib/Temporary Codes/screen.dart
@@ -8,9 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Home Page',
+      title: 'Home Page',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.white,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home:,
@@ -30,18 +30,18 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Welcome to Flutter!',
+              'Welcome to my home page!',
               style: TextStyle(
-                fontSize: 24.0,
+                fontSize: 44.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20.0),
+            SizedBox(height: 40.0),
             RaisedButton(
               onPressed: () {
                 // Add your onPressed action here
               },
-              child: Text('Click Me'),
+              child: Text('Click here'),
             ),
           ],
         ),
