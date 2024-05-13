d80466e2c2f29d96c39ae1521917680a76c74dc4
Momin-219 <123032309+Momin-219@users.noreply.github.com>
2024-05-14 00:50:05 +0500

    Create signup.dart

---
+++ lib/Temporary Codes/signup.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Signup Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SignupPage(),
+    );
+  }
+}
+
+class SignupPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void _signup() {
+    String username = _usernameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    // Here you can implement your signup logic, like calling an API to register the user
+
+    // For demonstration purposes, let's just print the user information
+    print('Username: $username');
+    print('Email: $email');
+    print('Password: $password');
+
+    // You can navigate to the next screen or perform any other action after signup
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Signup'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20.0),
+            RaisedButton(
+              onPressed: _signup,
+              child: Text('Signup'),
+              color: Colors.blue,
+              textColor: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
